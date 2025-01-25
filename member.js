b1bf831bb89d29d02ccc7202f5b35382be75b372
Mohsen Pazhoohiyani <mohsenpazhoohiyani@gmail.com>
2025-01-25 19:57:22 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -4,8 +4,10 @@ function skillsmember(){
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
-        x.style.display = "none";
+        x.style.display = "ghermez";
         x.style.display = "block";
     }
+
     
+
 }
